60033d903c8dd17168844f949abedaca78c12e51
Cayodias9 <cayod4048@gmail.com>
2022-04-04 17:20:22 +0000

    Salvando

--- Multiplos.java
+++ Multiplos.java
@@ -1,9 +1,9 @@
 public class Multiplos {
     public static void main(String[] args) {
 
-        for(int index = 0: index < 100; index++) {
+        for(int index = 0; index < 100; index++) {
             if (index % 6 == 0) {
-                System.out.println("Numero")
+                System.out.println("Numero multiplo de 6: " + index);
             }
         }
     }
